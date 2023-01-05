0088069f8344f4fc91632e5f8ff7c69ad287e36a
Anais1210 <azhang@myges.fr>
2023-01-05 11:46:31 +0100

    fetch asset done

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 const (
@@ -17,7 +19,57 @@ type Status struct {
 	}`json:"result"`
 }
 
-func main() {
+type Pair struct {
+	Altname string `json:"altname"`
+	Wsname string `json:"wsname"`
+	Base string `json:"base"`
+	Quote string `json:"quote"`
+	CostDecimals string `json:"cost_decimals"`
+	PairDecimals string `json:"pair_decimals"`
+}
+
+type Pairs struct{
+		PairList map[string]Pair `json:"result"`
+}
+
+
+func getPair(){
+	resp, err := http.Get(KrakenAPI + "/0/public/AssetPairs")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		return
+	}
+	//Write in file
+	
+	error := os.WriteFile("Archive/pairsKraken.csv", []byte(i.Altname), 0644)
+	if error != nil {
+		panic(error)
+	}
+
+	var m Pairs
+
+	errors := json.Unmarshal(body, &m)
+	if errors != nil {
+		fmt.Printf("Error: %s", errors)
+		return
+	}
+	var listOfPairs []string
+	for _, i := range m.PairList {
+		listOfPairs = append(listOfPairs, i.Altname)
+
+	}
+
+}
+func writeData(){
+
+}
+
+func getStatus(){
 	resp, err := http.Get(KrakenAPI + "/0/public/SystemStatus")
 	if err != nil {
 		panic(err)
@@ -38,6 +90,22 @@ func main() {
 		return
 	}
 
-
 	fmt.Printf("The current status of Kraken's API is %s", status.Result.Status)
+}
+func createFolder(){
+	err := os.Mkdir("Archive", 0750)
+	if err != nil && !os.IsExist(err) {
+		log.Fatal(err)
+	}
+	file, err := os.Create("Archive/pairsKakren.csv")
+	// err = os.WriteFile("Arhive/pairsKraken.csv", []byte("Hello, Gophers!"), 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+}
+func main() {
+	// getStatus()
+	getPair()
+	// createFolder()
 }

b63eb9a93f88a9874ab1ad0ae020872bf36f7363
Anais1210 <azhang@myges.fr>
2023-01-05 14:25:14 +0100

    write i csv file

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/anais1210/TP_FINAL_GO
 
 go 1.18
+
+require (
+	github.com/lib/pq v1.10.7 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
+
 
 const (
 	KrakenAPI = "https://api.kraken.com/"
@@ -24,12 +30,22 @@ type Pair struct {
 	Wsname string `json:"wsname"`
 	Base string `json:"base"`
 	Quote string `json:"quote"`
+	// CostDecimal int `json:"cost_decimals"`
+	// PairDecimal int `json:"pair_decimals"`
 }
 
 type Pairs struct{
-		PairList map[string]Pair `json:"result"`
+	PairList map[string]Pair `json:"result"`
 }
 
+const (
+	host     = "localhost"
+	port     = "5432"
+	user     = "toto"
+	password = "mysecretpassword"
+	dbname   = "mydatabase"
+	schema   = "public"
+)
 
 func getPair(){
 	resp, err := http.Get(KrakenAPI + "/0/public/AssetPairs")
@@ -42,12 +58,6 @@ func getPair(){
 		fmt.Printf("Error: %s", err)
 		return
 	}
-	//Write in file
-	error := os.WriteFile("Archive/pairsKraken.txt", body, 0644)
-	if error != nil {
-		panic(error)
-	}
-
 	var m Pairs
 
 	errors := json.Unmarshal(body, &m)
@@ -55,22 +65,45 @@ func getPair(){
 		fmt.Printf("Error: %s", errors)
 		return
 	}
+
+	// err := os.Mkdir("Archive", 0750)
+	// if err != nil && !os.IsExist(err) {
+	// 	log.Fatal(err)
+	// }
+	file, err := os.Create("Archive/pairsKraken.csv")
+	// err = os.WriteFile("Arhive/pairsKraken.csv", []byte("Hello, Gophers!"), 0660)
+	defer file.Close()
+    if err != nil {
+        log.Fatalln("failed to open file", err)
+    }
+    w := csv.NewWriter(file)
+    defer w.Flush()
+
+	header := []string{"altname", "wsname", "base", "quote"}
+
+	if err := w.Write(header); err != nil {
+        log.Fatalln("error writing csv:", err)
+    }
 	for _, i := range m.PairList {
-		fmt.Println(i.Altname)
+		var csvRow []string
+		csvRow = append(csvRow, i.Altname, i.Wsname,i.Base, i.Quote)
+        if err := w.Write(csvRow); err != nil {
+            log.Fatalln("error writing csv:", err)
+        }
+		// records := []string {
+		// 	{"altname", "wsname", "base", "quote"},
+		// 	{i.Altname, i.Wsname,i.Base, i.Quote},
+		// }
+		
+		// w.WriteAll(records)
 	}
-
 }
-func writeData(){
-
-}
-
 func getStatus(){
 	resp, err := http.Get(KrakenAPI + "/0/public/SystemStatus")
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	fmt.Println(resp.Body)
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -87,17 +120,35 @@ func getStatus(){
 
 	fmt.Printf("The current status of Kraken's API is %s", status.Result.Status)
 }
-func createFolder(){
-	err := os.Mkdir("Archive", 0750)
-	if err != nil && !os.IsExist(err) {
-		log.Fatal(err)
-	}
-	file, err := os.Create("Archive/pairsKakren.csv")
-	// err = os.WriteFile("Arhive/pairsKraken.csv", []byte("Hello, Gophers!"), 0660)
+
+func connectDatabase(datas []string){
+	connectionString := "host=" + host + " port=" + port + " user=" + user + " password=" + password + " dbname=" + dbname + " sslmode=disable"
+	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	defer file.Close()
+	defer db.Close()
+
+	sqlStat := "CREATE TABLE IF NOT EXISTS public.pairs ( id SERIAL NOT NULL, altname character varying NOT NULL, wsname character varying, base character varying, quote character varying, cost real, pair real ,PRIMARY KEY (id) ); ALTER TABLE IF EXISTS public.pairs OWNER to toto;"
+	_, errors := db.Exec(sqlStat)
+	if errors != nil {
+		fmt.Println(errors)
+	}
+
+	now := time.Now().Unix()
+	fmt.Println(datas)
+	for _, data := range datas {
+		// Insertion des données dans la base de données
+		sqlStatement := fmt.Sprintf("INSERT INTO pairs (id, altname) VALUES (%d, '%s')", now, data)
+		_, err := db.Exec(sqlStatement)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// Eviter les collisions de clé primaire
+		now++
+		// fmt.Println(data[i])
+	}
+
 }
 func main() {
 	// getStatus()
